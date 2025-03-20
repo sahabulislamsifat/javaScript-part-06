0dac3a081e38998155ae3361fdbc9423f1030883
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-21 04:24:15 +0600

    problem solving comming

---
+++ classes.js
@@ -0,0 +1,14 @@
+class Car {
+  constructor(names, year) {
+    this.names = names;
+    this.year = year;
+  }
+}
+
+const carOne = new Car("Toyota", 2027);
+const carTwo = new Car("BMW", 2021);
+const carThree = new Car("Audi", 2047);
+
+console.log(carOne);
+console.log(carTwo);
+console.log(carThree);
